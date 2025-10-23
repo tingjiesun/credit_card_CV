6876557f66c578fa20bf953612f1651968feb12c
tingjiesun <16670211213@163.com>
2025-10-23 22:47:44 +0800

    10.23_edit

--- template_matching_ocr/ocr_template_match.py
+++ template_matching_ocr/ocr_template_match.py
@@ -4,6 +4,7 @@ import numpy as np
 import argparse
 import cv2
 import myutils
+import os
 
 # 设置参数
 ap = argparse.ArgumentParser()
@@ -13,10 +14,14 @@ ap.add_argument("-t", "--template", required=True,
 	help="path to template OCR-A image")
 args = vars(ap.parse_args())
 
-a=args["template"]
-image = cv2.imdecode(np.fromfile(a, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
-cv2.imshow("My PNG Image", image)
-cv2.waitKey(0)
+# 个人增加：展示image/template 的图片
+#a=args["image"]
+#for filename in os.listdir(a):
+    #print(filename)
+    #img_path = os.path.join(a, filename)
+    #image = cv2.imdecode(np.fromfile(img_path, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+    #cv2.imshow("My PNG Image", image)
+    #cv2.waitKey(0)
 
 
 # 指定信用卡类型
@@ -26,31 +31,34 @@ FIRST_NUMBER = {
 	"5": "MasterCard",
 	"6": "Discover Card"
 }
+
+
 # 绘图展示
 def cv_show(name,img):
-	cv2.imshow(name, img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+    cv2.imshow(name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 # 读取一个模板图像
-img = cv2.imread(args["template"])
+img = cv2.imdecode(np.fromfile(args["template"], dtype=np.uint8), cv2.IMREAD_UNCHANGED)
 cv_show('img',img)
 # 灰度图
 ref = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv_show('ref',ref)
-# 二值图像
+# 二值图像(即黑白图）
 ref = cv2.threshold(ref, 10, 255, cv2.THRESH_BINARY_INV)[1]
 cv_show('ref',ref)
+
 
 # 计算轮廓
 #cv2.findContours()函数接受的参数为二值图，即黑白的（不是灰度图）,cv2.RETR_EXTERNAL只检测外轮廓，cv2.CHAIN_APPROX_SIMPLE只保留终点坐标
 #返回的list中每个元素都是图像中的一个轮廓
+refCnts, hierarchy = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-ref_, refCnts, hierarchy = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-cv2.drawContours(img,refCnts,-1,(0,0,255),3) 
+cv2.drawContours(img,refCnts,-1,(0,0,255),3)
 cv_show('img',img)
-print (np.array(refCnts).shape)
+#print (np.array(refCnts).shape)
 refCnts = myutils.sort_contours(refCnts, method="left-to-right")[0] #排序，从左到右，从上到下
 digits = {}
 
